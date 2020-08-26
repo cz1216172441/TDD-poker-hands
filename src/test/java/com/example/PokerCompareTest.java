e841173ff18de9ca5cb65b2220eee88529050bd2
CHENRO6 <chenro6@oocl.com>
2020-08-26 15:12:09 +0800

    [TDD-poker-hands][Roy, Fennie] refactor: should_black_wins_when_compare_given_black_2H_3D_5S_9C_10D_and_white_2C_3H_4S_8C_10S

--- src/test/java/com/example/PokerCompareTest.java
+++ src/test/java/com/example/PokerCompareTest.java
@@ -40,4 +40,27 @@ public class PokerCompareTest {
     assertEquals("White wins.", actual);
   }
 
+  @Test
+  void should_black_wins_when_compare_given_black_2H_3D_5S_9C_10D_and_white_2C_3H_4S_8C_10S() {
+    // given
+    List<Poker> black = new ArrayList<Poker>();
+    black.add(new Poker(2, 'H'));
+    black.add(new Poker(3, 'D'));
+    black.add(new Poker(5, 'S'));
+    black.add(new Poker(9, 'C'));
+    black.add(new Poker(10, 'D'));
+
+    List<Poker> white = new ArrayList<>();
+    white.add(new Poker(2, 'C'));
+    white.add(new Poker(3, 'H'));
+    white.add(new Poker(4, 'S'));
+    white.add(new Poker(8, 'C'));
+    white.add(new Poker(10, 'S'));
+
+    // when
+    String actual = pokerCompare.compare(black, white);
+    // then
+    assertEquals("Black wins.", actual);
+  }
+
 }
